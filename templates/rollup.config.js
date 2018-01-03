189647dd252d5f3ff58bd6e679618af24f28010e
Pedr Browne <pedr.browne@gmail.com>
2018-01-03 16:51:14 +0000

    build(rollup): Switch to single quotes

--- templates/rollup.config.js
+++ templates/rollup.config.js
@@ -1,39 +1,39 @@
-import nodeResolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  name: "#{project.dasherizedName}",
-  input: "src/index.js",
+  name: '#{project.dasherizedName}',
+  input: 'src/index.js',
   output: {
-    file: "dist/#{project.dasherizedName}.js",
-    format: "umd"
+    file: 'dist/#{project.dasherizedName}.js',
+    format: 'umd',
   },
   // Define modules that shouldn't be included in the build. It is assumed they
   // will be available via globals at runtime.
-  external: ["ramda"],
+  external: ['ramda'],
   // Define how external global modules should be referenced in the UMD bundle
-  globals: { ramda: "R" },
+  globals: { ramda: 'R' },
   plugins: [
     nodeResolve(),
     babel({
       babelrc: false,
-      exclude: "node_modules/**",
+      exclude: 'node_modules/**',
       presets: [
         [
-          "env",
+          'env',
           {
-            modules: false
-          }
-        ]
+            modules: false,
+          },
+        ],
       ],
       plugins: [
-        "external-helpers",
-        "transform-es2015-destructuring",
-        "transform-object-rest-spread"
-      ]
+        'external-helpers',
+        'transform-es2015-destructuring',
+        'transform-object-rest-spread',
+      ],
     }),
     // Allow CommonJS modules to be included in build.
-    commonjs()
-  ]
+    commonjs(),
+  ],
 };
